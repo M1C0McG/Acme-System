baa24357f0a6b8b79c596a95aee499d1283646b4
Mico McGovern <MicoMcGovern17@gmail.com>
2025-06-29 21:18:21 -0400

    heyNow complete

--- AcmeSystems/src/ver1/hello/HelloWorld.java
+++ AcmeSystems/src/ver1/hello/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hey now!");
+
+    public static void heyNowComplete() {
+        String HeyNow = "Hey Now Complete!";
+        System.out.println(HeyNow);
+}
     }
 }
